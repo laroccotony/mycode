b7b06292b21035d913df538f578f4afd7972820f
Tony LaRocco <tonylarocco94@gmail.com>
2024-03-26 09:27:26 -0700

    formatting

--- In-Class/code_repair.py
+++ In-Class/code_repair.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-mylist= [1,2,3,4,5, "Python"]
+mylist = [1,2,3,4,5, "Python"]
 
-name= input("What is your name?\n>")
+name = input("What is your name?\n>")
 
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
