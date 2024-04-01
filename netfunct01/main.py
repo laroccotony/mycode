b547be7c262bc79cd93aaf2f65a5fddebfa979b5
Tony LaRocco <tonylarocco94@gmail.com>
2024-04-01 10:50:50 -0700

    idk what just happened

---
+++ netfunct01/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+# python3 -m pip install crayons
+import crayons
+import json
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==dict
+
+    for ip in devicecmd.keys(): # looping through the dict
+        print(f'{crayons.red("Handshaking")}. .. ... connecting with {ip}') # fstring with RED
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'Attempting to sending command --> {mycmds}')
+            # we'll learn to write code that sends cmds to device here
+    return None
+
+# start our main script
+def main():
+    """called at runtime"""
+
+    with open("devicecmd.json", "r") as devj:
+        devicecmd = json.load(devj)
+
+    print(f"Welcome to the {crayons.blue('Network')} Device Command Pusher") # welcome message with blue text
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(devicecmd) # call function to push commands to devices
+
+def devicereboot(devicecmd):
+    for ip in devicecmd.keys():
+        print(f'{crayons.red("Handshaking")}. .. ... connecting with {ip}')
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'Attempting to sending command --> {mycmds}')
+            # we'll learn to write code that sends cmds to device here  
+    return None
+
+def devicejson():
+    pass
+    
+
+# call our main function
+main()
